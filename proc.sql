9c23711b4efa5ae827504415fa1c48e225af97b9
joonleon <55504618+joonleon@users.noreply.github.com>
2021-04-05 21:30:02 +0800

    Update proc.sql

--- proc.sql
+++ proc.sql
@@ -1241,39 +1241,58 @@ CREATE OR REPLACE FUNCTION get_available_instructors(cid int, start_date date, e
 RETURNS TABLE(eid int, name text, num_hours int, day date, available_hours time []) AS $$
 DECLARE
 	curs CURSOR FOR (
-	select distinct S2.eid, I1.name
-	from Specializes S2, Courses C1, Instructors I1
+	(select S2.eid, I1.name
+	from Specializes S2, Courses C1, Instructors I1, Employees E1
 	where I1.eid = S2.eid
 	and I1.eid = S1.eid
 	and S2.name = C1.name
 	and C1.course_id = cid
-	order by eid asc
-	);
+	and E1.depart_date IS NULL
+	EXCEPT
+	select eid, name
+	from (Part_time_Instructors natural join Instructors) PI
+	where (
+		select sum(DATE_PART('hour', end_time - start_time))
+		from Sessions
+		where eid = PI.eid
+		and course_id = cid
+		and session_date >= start_date
+		and session_date <= end_date
+		and extract(month from session_date) = extract(month from start_date)
+		and extract(year from session_date) = extract(year from start_date)
+	) >= 30
+	)order by eid asc);
 	r RECORD;
 	start_day date;
 	start_hour time;
-	num_part_time_hours int;
 
 BEGIN
 	OPEN curs;
 	LOOP
 		FETCH curs INTO r;
 		EXIT WHEN NOT FOUND;
+				
+				eid := r.eid;
+				name := r.name;
 				select sum(DATE_PART('hour', end_time - start_time) ) into num_hours
 				from Sessions S1
 				where S1.eid = r.eid
-				and extract(month from S1.session_date) = extract(month from start_day)
-				and extract(year from S1.session_date) = extract(year from start_day);
-			
-			IF EXISTS (select 1 from Part_time_Instructors where eid = r.eid) and num_hours > 30 THEN
-
-			ELSE
-				eid := r.eid;
-				name := r.name;
+				and extract(month from S1.session_date) = extract(month from start_date)
+				and extract(year from S1.session_date) = extract(year from start_date);
 				start_day := start_date;
 			
 				while start_day <= end_date
 				LOOP
+
+					IF extract(dow from start_day) = 6 THEN
+						start_day := start_day + '2 days'::interval;
+						CONTINUE;
+					END IF;
+
+					IF extract(dow from start_day) = 0 THEN
+						start_day := start_day + '1 day'::interval;
+						CONTINUE;
+					END IF;
 
 					day := start_day;
 
@@ -1316,8 +1335,7 @@ BEGIN
 
 					start_day := start_day + '1 day'::interval;
 
-				END LOOP;
-			END IF;
+				END LOOP;		
 	END LOOP;
 	CLOSE curs;
 END;
