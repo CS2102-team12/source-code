886af209293e433d3caace4028d87e2da0dc3a67
joonleon <55504618+joonleon@users.noreply.github.com>
2021-04-06 17:34:03 +0800

    Update data.sql

--- data.sql
+++ data.sql
@@ -37,6 +37,33 @@ CALL add_customer ('Catlaina Roxbrough','3984 Melby Way','854-297-3829','croxbro
 CALL add_customer ('Raddy MacKeig','480 Del Mar Center','412-919-7082','rmackeigd@walmart.com',374288779575975,'2022-12-12',53);
 CALL add_customer ('Bram Schenfisch','844 Daystar Pass','382-685-5211','bschenfische@nbcnews.com',348024809032247,'2021-09-13',331);
 
+call add_employee('Joon Leon', '770 Pine View Lane', '82910192', 'jl@gmail.com', '2020-01-09', 'full_time', 80000, 'Manager', array['Chemistry','Physics', 'Human Resource']);
+call add_employee('Joon Lee', '0588 Crownhardt Crossing', '82193819', 'jl2@gmail.com', '2021-01-01', 'full_time', 1029, 'Administrator', null);
+call add_employee('Joseph', '8880 Merry Parkway', '92819820', 'jjj99@hotmail.com', '2006-09-09', 'full_time', 99999, 'Manager', array['a', 'b', 'c']);
+call add_employee('Iowa', '6 Brentwood Avenue', '10010100', 'iowal@gmail.com', '2008-06-07', 'full_time', 100000, 'Manager', array['p', 'q', 'r', 's']);
+call add_employee('James', '93234 Golf View Plaza','10930184','james@gmail.com','2020-01-09','full_time',1209,'Administrator',null);
+call add_employee('Jason', '3 Spaight Hill', '131314154', 'jason@gmail.com', '2020-01-09', 'full_time', 130193, 'Administrator',null);
+call add_employee('Zook',	'28140 Stang Avenue', '13981331', 'zzzzz9191@gmail.com', '2008-09-06', 'full_time',	9138, 'Administrator',null);
+call add_employee('Bob the builder',	'1109 Cottonwood Parkway',	'131138913', 'b0b@gmail.com', '2013-03-10', 'full_time', 894, 'Instructor', array['Physics', 'Human Resource']);
+call add_employee('Yam', '04 Donald Park', '397592845', 'purple@gmail.com', '2013-02-28', 'full_time', 8394, 'Instructor', array['a', 'b']);
+call add_employee('Anna',	'74 Autumn Leaf Crossing', '29875933', 'naan@gmail.com', '2012-02-28', 'full_time', 13091, 'Instructor', array['Chemistry', 'c']);
+call add_employee('Kay',	'93 Brentwood Place','30987538', 'okay@gmail.com', '2020-01-09', 'full_time', 444, 'Instructor', array['p', 'q']);
+call add_employee('Onion', '915 Drewry Court', '98340988', 'pink@gmail.com', '2018-01-01', 'part_time', 5,'Instructor', array['r', 's']);
+call add_employee('Garlic', '6 Westerfield Pass',  '19292348', 'smells_great@gmail.com',	'2017-09-11', 'part_time', 5, 'Instructor', array['a', 'b', 'c', 'p', 'q', 'r', 's']);
+call add_employee('Ginger', '58 Esker Place', '42492420',	'ginger@gmail.com',	'2021-01-02', 'part_time', 10,'Instructor', array['p', 'q', 'r', 's']);
+call add_employee('Pepper', '39509 Veith Trail', '94024848', 'pepper9@gmail.com',	'2020-12-31', 'part_time', 100, 'Instructor', array['Human Resource']);
+                                                                                                                                        
+insert into Course_areas values ('Chemistry',	1);
+insert into Course_areas values ('Physics',	1);
+insert into Course_areas values ('Human Resource',	1);
+insert into Course_areas values ('a',3);
+insert into Course_areas values ('b', 3);
+insert into Course_areas values ('c', 3);
+insert into Course_areas values ('p',4);
+insert into Course_areas values ('q',4);
+insert into Course_areas values ('r',4);
+insert into Course_areas values ('s',3);
+
 CALL add_course_package ('A',10,'2021-01-01','2021-12-31',10922);
 CALL add_course_package ('B',11,'2021-02-01','2021-03-03',30190);
 CALL add_course_package ('C',1290,'2021-01-01','2023-12-30',10933);
